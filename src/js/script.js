ed7b27a92ba13e476676335f475cdc20ba8f8244
MikhailGryschenko <maybeminsk@gmail.com>
2022-08-10 21:42:09 +0300

    animation

--- src/js/script.js
+++ src/js/script.js
@@ -79,4 +79,34 @@ $(document).ready(function(){
     validateForms ('#order form');
 
     $('input[name=phone]').mask("+375 (99) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        if (!$(this).validate()) {
+            return;
+        }
+        $.ajax( {
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    // Smooth Scroll and pageup
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
 });
